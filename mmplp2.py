45a99f162497bc616e0336a40538c1e1077a99aa
Erica Mays <erica@emays.dev>
2021-08-09 17:07:33 -0400

    Add argument parsing; allow multiple apportionments.
    * Add argument parsing; more flags may be added in the future to
      override JSON definition data.
    * Create a table that allows for multiple apportionment methods.
      Currently only supports Germany.

--- mmplp2.py
+++ mmplp2.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
+import argparse
+import textwrap
+
 from pprint import pprint
 
 import common
 import german
 
 
+run_election_types = {
+    'Germany': german.run_german_election
+}
+
+
 if __name__ == '__main__':
-    election = common.load_election('ballots/de2017.json')
-    results = german.run_german_election(election)
-    pprint(results)
+    parser = argparse.ArgumentParser(description='Run various types of elections.')
+    parser.add_argument('definition', help='Definition file for the election.')
+
+    args = parser.parse_args()
+
+    election = common.load_election(args.definition)
+
+    try:
+        fun = run_election_types[election['apportionment']]
+        results = fun(election)
+        pprint(results)
+    except KeyError:
+        print(textwrap.dedent(f"""\
+        Invalid apportionment method "{election['apportionment']}"
+
+        Valid apportionment methods are: {', '.join(run_election_types.keys())}
+        """).strip())
